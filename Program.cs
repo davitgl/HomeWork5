0941542144e7027d7ad832e44e5e092884e0c171
davitgl <dglonti3@gmail.com>
2024-09-26 20:25:23 +0400

    percent check

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace app4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+        }
+    }
+}
